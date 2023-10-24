d6cdf5be0b2ea027b30e69cdb42a4f31e01d3855
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:59:00 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_gcf_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for GCF Calculator by Calculator.iO";
 
 function display_ci_gcf_calculator(){
